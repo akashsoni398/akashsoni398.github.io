756c73511b21bca4bdb8b9122b73633259b08658
SuperSonicYT <84252471+SuperSonicYT@users.noreply.github.com>
2023-05-05 06:46:59 +0530

    3

--- js/main.js
+++ js/main.js
@@ -14,34 +14,6 @@
     
     // Initiate the wowjs
     new WOW().init();
-
-
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-    
     
     // Back to top button
     $(window).scroll(function () {
